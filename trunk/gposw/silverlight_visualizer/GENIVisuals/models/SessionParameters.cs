013f1dc7336ca1d3ec2ddfc9c788085e72cc8b23
Wenjie Zeng <wzeng@bbn.com>
2010-09-08 17:20:46 +0000

    git-svn-id: http://groups.geni.net/svn/syseng@371 b5a3a339-7797-dc11-ac77-001cc43a1592

--- trunk/gposw/silverlight_visualizer/GENIVisuals/models/SessionParameters.cs
+++ trunk/gposw/silverlight_visualizer/GENIVisuals/models/SessionParameters.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net;
 using System.Windows;
+using System.Collections.Generic;
 using System.Windows.Controls;
 using System.Windows.Documents;
 using System.Windows.Ink;
@@ -13,6 +14,24 @@ namespace GENIVisuals.models
 {
     public class SessionParameters
     {
+        public SessionParameters(SessionParameters copy)
+        {
+            slice = copy.slice;
+            dbHost = copy.dbHost;
+            dbUser = copy.dbUser;
+            dbPassword = copy.dbPassword;
+            dbName = copy.dbName;
+            useDebugServer = copy.useDebugServer;
+            debugServer = copy.debugServer;
+            useBogusData = copy.useBogusData;
+            makePeriodicQuery = copy.makePeriodicQuery;
+            topologyVisuals = new List<Visual>(copy.topologyVisuals);
+        }
+
+        public SessionParameters() {
+            topologyVisuals = new List<Visual>();
+        }
+
         public string slice { get; set; }
         public string dbHost { get; set; }
         public string dbUser { get; set; }
@@ -21,5 +40,7 @@ namespace GENIVisuals.models
         public bool useDebugServer { get; set; }
         public string debugServer { get; set; }
         public bool useBogusData { get; set; }
+        public bool makePeriodicQuery { get; set; }
+        public List<Visual> topologyVisuals { get; set; }
     }
 }
