7c07ef2cd89a898719df0603d8907247d323f1f9
Wenjie Zeng <wzeng@bbn.com>
2010-09-16 17:59:02 +0000

    git-svn-id: http://groups.geni.net/svn/syseng@383 b5a3a339-7797-dc11-ac77-001cc43a1592

--- trunk/gposw/silverlight_visualizer/GENIVisuals/ChartPage.xaml.cs
+++ trunk/gposw/silverlight_visualizer/GENIVisuals/ChartPage.xaml.cs
@@ -26,6 +26,7 @@ namespace GENIVisuals
         public ObservableCollection<SampleDataItem> PageViewData { get { return m_pageViewData; } }
 
         private bool m_pause = false;
+        private double m_updateInterval = 500;
 
         private void UserControl_Loaded(object sender, RoutedEventArgs e)
         {
@@ -34,7 +35,10 @@ namespace GENIVisuals
             m_pageViewData = pageViewDataLayer.Data;
 
             DispatcherTimer dt = new DispatcherTimer();
-            dt.Interval = new TimeSpan(0, 0, 0, 0, 500); // dy, hr, min, sec, ms
+            int min = (int)(m_updateInterval / (1000 * 60));
+            int sec = (int)(m_updateInterval / 1000) - min * 60;
+            int ms = (int)m_updateInterval - min * 60 * 1000 - sec * 1000;
+            dt.Interval = new TimeSpan(0, 0, min, sec, ms); // dy, hr, min, sec, ms
             dt.Tick += new EventHandler(RequestNextUpdate);
             dt.Start();
 
@@ -53,6 +57,24 @@ namespace GENIVisuals
         public string Title {
             get { return m_title.Text; } 
             set { m_title.Text = value; } 
+        }
+
+        public string Graph1Title
+        {
+            get { return lineGraph1.Title; }
+            set { m_title.Text = value; } 
+        }
+
+        public string Graph2Title
+        {
+            get { return lineGraph2.Title; }
+            set { m_title.Text = value; }
+        }
+
+        public double UpdateFrequency
+        {
+            get { return m_updateInterval; }
+            set { m_updateInterval = value; }
         }
 
         public void AddDataPoint(double rxThroughput, double txThroughput, string timestamp)
