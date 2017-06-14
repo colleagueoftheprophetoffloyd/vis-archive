a05e89adffe03941bf848d825ddb09cbf0746552
Mark Berman <mberman@acm.org>
2017-06-14 14:13:33 -0400

    Cleaned up old branches and unused directories.

--- tags/gposw/silverlight_visualizer/GEC9/GENIVisuals.Web/php/sql_vars_links.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Table and Column Names
-$mysql_linksTable  = "links";
-$mysql_IDCol = "id";
-$mysql_nameCol = "name";
-$mysql_sourceNodeCol = "sourceNode";
-$mysql_destNodeCol = "destNode";
-?>
-
